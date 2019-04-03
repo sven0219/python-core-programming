9e03b1bb16669fd9f753ee27b4c6d43df0eec88b
沈文强 <wenqiang.shen@qiyuesuo.com>
2019-04-03 15:25:03 +0800

    添加第二章前四节

---
+++ chapter02/2_1_print.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 程序输出
+myString = '你好，世界！'
+print(myString)
+
+print('%s is number %d!' % ("Python",1))
---
+++ chapter02/2_2_raw_input.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# python2.x 中raw_input()和input()都存在，前者将所有输入当作字符串看待,后者将所有输入当作数字
+# python3中将raw_input()和input()合并
+
+user = input("Enter login name:")
+print('Your login name is :',user)
+
+
