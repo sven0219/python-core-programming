8a3b73696d403dca2c557fa92ad8db2f004f9b46
沈文强 <wenqiang.shen@qiyuesuo.com>
2019-04-27 08:02:30 +0800

    Add chapter07 7.1

--- chapter07/7_1_userPassWord.py
+++ chapter07/7_1_userPassWord.py
@@ -5,7 +5,7 @@ db = {}
 
 
 def newuser():
-    prompt = "Login desired!"
+    prompt = "Login desired:"
     while True:
         name = input(prompt)
         # Python 3.x不再支持 has_key() 函数，而被__contains(‘keyname’)所替代
@@ -15,7 +15,47 @@ def newuser():
         else:
             break
 
-        pwd = input("Password: ")
-        db[name] = pwd
+    pwd = input("Password: ")
+    db[name] = pwd
 
 
+def olduser():
+    name = input("login: ")
+    pwd = input("password: ")
+    passWord = db.get(name)
+    if pwd == passWord:
+        print("Welcome back,", name)
+    else:
+        print("Login incorrect!")
+
+
+def showmenu():
+    prompt = """
+(N)ew User Login
+(E)xisting User Login
+(Q)uit
+Enter choice: """
+
+    done = 0
+    while not done:
+        chosen = 0
+        while not chosen:
+            try:
+                choice = input(prompt)[0]
+            except (EOFError, KeyboardInterrupt):
+                choice = 'q'
+            print
+            '\nYou picked: [%s]' % choice
+
+            if choice not in 'neq':
+                print("invalid menu option, try again")
+            else:
+                chosen = 1
+
+        if choice == 'q': done = 1
+        if choice == 'n': newuser()
+        if choice == 'e': olduser()
+
+
+if __name__ == '__main__':
+    showmenu()
