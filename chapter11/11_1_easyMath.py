20f92ec1f3ce3bdc7fabff576a3c35fa651460cd
沈文强 <wenqiang.shen@qiyuesuo.com>
2019-07-15 14:21:35 +0800

    add 11_1

--- chapter11/11_1_easyMath.py
+++ chapter11/11_1_easyMath.py
@@ -1,7 +1,48 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+__author__ = 'xy'
 
-from operator import add,sub
-from random import randint,choice
+import random
+import operator
 
-ops = {'+' : add,'-':sub}
+
+ops = {'+': operator.add, '-': operator.sub}
+MAXTRIES = 2
+
+
+def doprob():
+    op = random.choice('+-')
+    nums = [random.randint(1, 10) for i in range(2)]
+    nums.sort(reverse=True)
+    ans = ops[op](*nums)
+    pr = '%d %s %d =' % (nums[0], op, nums[1])
+    print(pr)
+    oops = 0
+    while True:
+        try:
+            if int(input()) == ans:
+                print('correct')
+                break
+            if oops == MAXTRIES:
+                print ('answer : %s%s' % (pr, ans))
+                break
+            else:
+                print ('incorrect... try again')
+                oops += 1
+        except (KeyboardInterrupt, EOFError, ValueError):
+            print ('invalid input... try again')
+
+
+def main():
+    while True:
+        doprob()
+        try:
+            opt = input('Again? [y/n]').lower()
+            if opt and opt[0] == 'n':
+                break
+        except (KeyboardInterrupt, EOFError):
+            break
+
+
+if __name__ == '__main__':
+    main()
